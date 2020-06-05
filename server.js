eedc3d44144748ae2cca348888042f93f1603911
Santiago Ruiz <santiago.aruiz28@gmail.com>
2020-06-05 16:13:48 -0500

    intent 32232

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
+//Install express server
 const express = require('express');
-const path = require('path')
+const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/task-app'));
 
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/task-app/index.html'));
+});
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-//path location strategy
-
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
-
-console.log('Console is listening!')
